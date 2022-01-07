6dbbc0cb1748ceb586816a9dc2503a5a2366cce2
moritz1029 <81555199+moritz1029@users.noreply.github.com>
2022-01-07 02:56:50 +0100

    compatibility with old sodium

---
+++ src/main/java/me/voidxwalker/worldpreview/OldSodiumCompatibility.java
@@ -0,0 +1,14 @@
+package me.voidxwalker.worldpreview;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockRenderView;
+
+import java.util.Random;
+
+public interface OldSodiumCompatibility {
+    public boolean renderSafe(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack matrix, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay) ;
+}
