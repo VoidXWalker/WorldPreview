07c2b9ef3780aa278a1bddbd5ade0a7dfb267a9e
Wurgo <71964154+wurgo@users.noreply.github.com>
2022-01-26 10:34:46 +0100

    Update BlockModelRendererMixin.java

--- src/main/java/me/voidxwalker/worldpreview/mixin/BlockModelRendererMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/BlockModelRendererMixin.java
@@ -21,7 +21,6 @@ import java.util.Random;
 @Mixin(BlockModelRenderer.class)
 public abstract class BlockModelRendererMixin implements OldSodiumCompatibility {
     @Shadow public abstract boolean renderSmooth(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay);
-
     @Shadow public abstract boolean renderFlat(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long l, int i);
 
     @Override
@@ -29,16 +28,13 @@ public abstract class BlockModelRendererMixin implements OldSodiumCompatibility 
         boolean bl = MinecraftClient.isAmbientOcclusionEnabled() && state.getLuminance() == 0 && model.useAmbientOcclusion();
         Vec3d vec3d = state.getModelOffset(world, pos);
         matrix.translate(vec3d.x, vec3d.y, vec3d.z);
-        try {
-            return bl ? this.renderSmooth(world, model, state, pos, matrix, vertexConsumer, cull, random, seed, overlay) : this.renderFlat(world, model, state, pos, matrix, vertexConsumer, cull, random, seed, overlay);
-        } catch (Throwable var17) {
+        try { return bl ? this.renderSmooth(world, model, state, pos, matrix, vertexConsumer, cull, random, seed, overlay) : this.renderFlat(world, model, state, pos, matrix, vertexConsumer, cull, random, seed, overlay); }
+        catch (Throwable var17) {
             CrashReport crashReport = CrashReport.create(var17, "Tesselating block model");
             CrashReportSection crashReportSection = crashReport.addElement("Block model being tesselated");
             CrashReportSection.addBlockInfo(crashReportSection, pos, state);
-            crashReportSection.add("Using AO", (Object)bl);
+            crashReportSection.add("Using AO", (Object) bl);
             throw new CrashException(crashReport);
         }
     }
-
-    
 }
