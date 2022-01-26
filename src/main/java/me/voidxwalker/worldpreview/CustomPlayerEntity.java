a3b9d270c302221c0ca4f43dc625041fd6aed9e7
Wurgo <71964154+wurgo@users.noreply.github.com>
2022-01-26 10:33:29 +0100

    Update CustomPlayerEntity.java

--- src/main/java/me/voidxwalker/worldpreview/CustomPlayerEntity.java
+++ src/main/java/me/voidxwalker/worldpreview/CustomPlayerEntity.java
@@ -11,22 +11,17 @@ public class CustomPlayerEntity extends Entity  {
     public boolean calculatedSpawn;
     public CustomPlayerEntity(EntityType<?> type, World world, BlockPos pos,float yaw, float pitch) {
         super(type, world);
-        this.setPos(pos.getX(),pos.getY(),pos.getZ());
-        calculatedSpawn=false;
-        this.yaw=yaw;
-        this.pitch=pitch;
-        this.prevX=pos.getX();
-        this.prevY=pos.getY();
-        this.prevZ=pos.getZ();
+        this.setPos(pos.getX(), pos.getY(), pos.getZ());
+        calculatedSpawn = false;
+        this.yaw = yaw;
+        this.pitch = pitch;
+        this.prevX = pos.getX();
+        this.prevY = pos.getY();
+        this.prevZ = pos.getZ();
     }
 
     @Override protected void initDataTracker() {}
-
     @Override protected void readCustomDataFromTag(CompoundTag tag) {}
-
     @Override protected void writeCustomDataToTag(CompoundTag tag) {}
-
-    @Override public Packet<?> createSpawnPacket() {
-        return null;
-    }
+    @Override public Packet<?> createSpawnPacket() { return null; }
 }
