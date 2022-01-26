33bb3b37e426cfe04b7e7e1ab2b8908e3192922d
Wurgo <71964154+wurgo@users.noreply.github.com>
2022-01-26 10:58:25 +0100

    Update MinecraftClientMixin.java

--- src/main/java/me/voidxwalker/worldpreview/mixin/MinecraftClientMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/MinecraftClientMixin.java
@@ -21,59 +21,48 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
-
     @Shadow protected abstract void render(boolean tick);
-
     @Shadow private @Nullable IntegratedServer server;
-
     @Shadow @Nullable public Entity cameraEntity;
     @Shadow private @Nullable ClientConnection connection;
     @Shadow @Final private SoundManager soundManager;
     @Shadow private Profiler profiler;
-
     @Shadow protected abstract void reset(Screen screen);
-
     private int cycleCooldown;
 
     @Inject(method = "startIntegratedServer(Ljava/lang/String;Lnet/minecraft/util/registry/RegistryTracker$Modifiable;Ljava/util/function/Function;Lcom/mojang/datafixers/util/Function4;ZLnet/minecraft/client/MinecraftClient$WorldLoadAction;)V",at=@At(value = "INVOKE",shift = At.Shift.AFTER,target = "Lnet/minecraft/server/integrated/IntegratedServer;isLoading()Z"),cancellable = true)
-    public void onHotKeyPressed( CallbackInfo ci){
-        if(WorldPreview.inPreview){
-
+    public void onHotKeyPressed(CallbackInfo ci) {
+        if (WorldPreview.inPreview){
             cycleCooldown++;
-            if(WorldPreview.resetKey.wasPressed()|| WorldPreview.kill==-1){
-
-                WorldPreview.log(Level.INFO,"Leaving world generation");
+            if (WorldPreview.resetKey.wasPressed() || WorldPreview.kill == -1) {
+                WorldPreview.log(Level.INFO, "Leaving world generation");
                 WorldPreview.kill = 1;
-                while(WorldPreview.inPreview){
-                    Thread.yield();
-
-                }
+                while (WorldPreview.inPreview) { Thread.yield(); }
                 this.server.shutdown();
                 MinecraftClient.getInstance().disconnect();
-                WorldPreview.kill=0;
+                WorldPreview.kill = 0;
                 MinecraftClient.getInstance().openScreen(new TitleScreen());
                 ci.cancel();
             }
-            if(WorldPreview.stopKey.wasPressed()&&!WorldPreview.stop){
-
-                WorldPreview.inPreview=false;
-                WorldPreview.stop=true;
-                WorldPreview.log(Level.INFO,"Hiding Preview");
+            
+            if (WorldPreview.stopKey.wasPressed() && !WorldPreview.stop) {
+                WorldPreview.inPreview = false;
+                WorldPreview.stop = true;
+                WorldPreview.log(Level.INFO, "Hiding Preview");
             }
-            if(WorldPreview.cycleChunkMapKey.wasPressed()&&cycleCooldown>10&&!WorldPreview.stop){
-                cycleCooldown=0;
-                WorldPreview.chunkMapPos= WorldPreview.chunkMapPos<5? WorldPreview.chunkMapPos+1:1;
+            if (WorldPreview.cycleChunkMapKey.wasPressed() && cycleCooldown > 10 && !WorldPreview.stop){
+                cycleCooldown = 0;
+                WorldPreview.chunkMapPos = WorldPreview.chunkMapPos < 5 ? WorldPreview.chunkMapPos + 1:1;
             }
         }
     }
 
-    @Inject(method="startIntegratedServer(Ljava/lang/String;)V",at=@At(value = "HEAD"))
-    public void isExistingWorld(String worldName, CallbackInfo ci){
-        WorldPreview.existingWorld=true;
-    }
+    @Inject(method="startIntegratedServer(Ljava/lang/String;)V", at = @At(value = "HEAD"))
+    public void isExistingWorld(String worldName, CallbackInfo ci) { WorldPreview.existingWorld = true; }
+    
     @Redirect(method="joinWorld",at=@At(value="INVOKE",target="Lnet/minecraft/client/MinecraftClient;reset(Lnet/minecraft/client/gui/screen/Screen;)V"))
-    public void smoothTransition(MinecraftClient instance, Screen screen){
-        if(!WorldPreview.stop){
+    public void smoothTransition(MinecraftClient instance, Screen screen) {
+        if (!WorldPreview.stop) {
             this.profiler.push("forcedTick");
             this.soundManager.stopAll();
             this.cameraEntity = null;
@@ -81,23 +70,18 @@ public abstract class MinecraftClientMixin {
             this.render(false);
             this.profiler.pop();
         }
-        else {
-            this.reset(screen);
-        }
-
+        else { this.reset(screen); }
     }
-    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;)V",at=@At(value = "HEAD"))
-    public void reset(Screen screen, CallbackInfo ci){
-        synchronized (WorldPreview.lock){
-            WorldPreview.world=null;
-            WorldPreview.player=null;
-            WorldPreview.clientWord=null;
-            WorldPreview.camera=null;
-            if(WorldPreview.worldRenderer!=null){
-                WorldPreview.worldRenderer.loadWorld(null);
-            }
+    
+    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;)V", at = @At(value = "HEAD"))
+    public void reset(Screen screen, CallbackInfo ci) {
+        synchronized (WorldPreview.lock) {
+            WorldPreview.world = null;
+            WorldPreview.player = null;
+            WorldPreview.clientWord = null;
+            WorldPreview.camera = null;
+            if (WorldPreview.worldRenderer != null) { WorldPreview.worldRenderer.loadWorld(null); }
             cycleCooldown=0;
         }
-
     }
 }
