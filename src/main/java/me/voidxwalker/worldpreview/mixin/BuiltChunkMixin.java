5e37803de30f72fb5bfb39ef1961ab13addae3e8
Wurgo <71964154+wurgo@users.noreply.github.com>
2022-01-26 10:36:03 +0100

    Update BuiltChunkMixin.java

--- src/main/java/me/voidxwalker/worldpreview/mixin/BuiltChunkMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/BuiltChunkMixin.java
@@ -12,13 +12,9 @@ import java.util.Collection;
 
 @Mixin(ChunkBuilder.BuiltChunk.class)
 public class BuiltChunkMixin {
-    @Redirect(method = "setNoCullingBlockEntities",at=@At(value = "INVOKE",target = "Lnet/minecraft/client/render/WorldRenderer;updateNoCullingBlockEntities(Ljava/util/Collection;Ljava/util/Collection;)V"))
+    @Redirect(method = "setNoCullingBlockEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;updateNoCullingBlockEntities(Ljava/util/Collection;Ljava/util/Collection;)V"))
     public void sodiumCompatibility(WorldRenderer instance, Collection<BlockEntity> removed, Collection<BlockEntity> added){
-        if(instance==null){
-            WorldPreview.worldRenderer.updateNoCullingBlockEntities(removed, added);
-        }
-        else {
-            instance.updateNoCullingBlockEntities(removed,added);
-        }
+        if(instance==null) { WorldPreview.worldRenderer.updateNoCullingBlockEntities(removed, added); }
+        else { instance.updateNoCullingBlockEntities(removed, added); }
     }
 }
