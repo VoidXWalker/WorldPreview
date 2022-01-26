442904dfc5b982e0a5b4077940584b0942596384
Wurgo <71964154+wurgo@users.noreply.github.com>
2022-01-26 10:45:52 +0100

    Update KeyboardMixin.java

--- src/main/java/me/voidxwalker/worldpreview/mixin/KeyboardMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/KeyboardMixin.java
@@ -15,22 +15,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Keyboard.class)
 public abstract class KeyboardMixin {
     @Shadow @Final private MinecraftClient client;
-
     @Shadow protected abstract boolean processF3(int key);
 
-    @Inject(method = "onKey",at=@At("HEAD"))
-    public void getF3ESCKey(long window, int key, int scancode, int i, int j, CallbackInfo ci){
-        if( WorldPreview.inPreview&&window == this.client.getWindow().getHandle()){
-            if(i!=0) {
-
+    @Inject(method = "onKey", at = @At("HEAD"))
+    public void getF3ESCKey(long window, int key, int scancode, int i, int j, CallbackInfo ci) {
+        if (WorldPreview.inPreview && window == this.client.getWindow().getHandle()) {
+            if (i != 0) {
                 InputUtil.Key key2 = InputUtil.fromKeyCode(key, scancode);
-                if (key == 256&& InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), 292)) {
-                    WorldPreview.showMenu=!WorldPreview.showMenu;
-                }
+                if (key == 256 && InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), 292)) { WorldPreview.showMenu = !WorldPreview.showMenu; }
+                
                 boolean bl2 = InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), 292) && this.processF3(key);
-                if (bl2) {
-                    KeyBinding.setKeyPressed(key2, false);
-                }
+                if (bl2) { KeyBinding.setKeyPressed(key2, false); }
                 else {
                     KeyBinding.setKeyPressed(key2, true);
                     KeyBinding.onKeyPressed(key2);
