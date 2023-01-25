9797c7413245355f91785a9dd84682ff5969e7e9
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2023-01-25 16:56:18 -0400

    Move chunk map before preview

--- src/main/java/me/voidxwalker/worldpreview/mixin/client/render/LevelLoadingScreenMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/client/render/LevelLoadingScreenMixin.java
@@ -56,16 +56,10 @@ public abstract class LevelLoadingScreenMixin extends Screen {
     }
     @ModifyVariable(method = "render", at = @At("STORE"), ordinal = 2)
     public int moveLoadingScreen(int i){
-        if(WorldPreview.camera==null){
-            return i;
-        }
         return worldpreview_getChunkMapPos().x;
     }
     @ModifyVariable(method = "render", at = @At("STORE"), ordinal = 3)
     public int moveLoadingScreen2(int i){
-        if(WorldPreview.camera==null){
-            return i;
-        }
         return worldpreview_getChunkMapPos().y;
     }
     @Inject(method = "render",at=@At("HEAD"))
